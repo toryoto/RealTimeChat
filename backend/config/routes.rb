81aad1197b8c6f204c49c75618e0e2f26a440ed4
toryoto <s1f102002345@iniad.org>
2024-05-12 02:11:47 +0900

    ルートの変更

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  constraints format: :json do
+  namespace :api, format: :json do
     mount_devise_token_auth_for 'User', at: 'auth', controllers: {
       registrations: 'auth/registrations'
     }
