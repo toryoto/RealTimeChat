c7fdd94c5a504f60faf22516316361abd82ee37b
toryoto <s1f102002345@iniad.org>
2024-06-05 23:44:53 +0900

    is_email_publicの値を反転する

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -1,15 +1,19 @@
 module Api
   class UsersController < ApplicationController
-    before_action :set_user, only: [:show]
+    before_action :set_user, only: [:show, :update_email_visibility]
 
     def show
       render json: @user
     end
 
     def update_email_visibility
-      if @user.update(user_params)
+      new_visibility = !@user.is_email_public
+  
+      if @user.update(is_email_public: new_visibility)
         render json: @user, status: :ok
       else
+        # バリデーションエラーメッセージのログ
+        Rails.logger.info(@user.errors.full_messages)
         render json: @user.errors, status: :unprocessable_entity
       end
     end
@@ -18,6 +22,8 @@ module Api
 
     def set_user
       @user = User.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'User not found' }, status: :not_found
     end
 
     def user_params
