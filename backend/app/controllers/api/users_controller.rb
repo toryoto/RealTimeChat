a9ff993117451b45927529dc2bf86ed2f475947d
toryoto <s1f102002345@iniad.org>
2024-06-05 11:42:13 +0900

    update_email_visibilityAPIの作成

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -6,10 +6,22 @@ module Api
       render json: @user
     end
 
+    def update_email_visibility
+      if @user.update(user_params)
+        render json: @user, status: :ok
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+
     private
 
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:is_email_public)
     end
   end
 end
